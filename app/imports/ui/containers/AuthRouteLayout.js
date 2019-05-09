7094bfe701f9512e93edd0e73d4cddc7613f41d1
Philip Nunoo <phanunoo@gmail.com>
2019-05-09 11:06:18 +0000

    redirect when user is logged in from auth route layout

--- app/imports/ui/containers/AuthRouteLayout.js
+++ app/imports/ui/containers/AuthRouteLayout.js
@@ -1,27 +1,59 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Route } from 'react-router-dom';
+import { withRouter } from 'react-router';
+import { Meteor } from 'meteor/meteor';
+import { Tracker } from 'meteor/tracker';
 
-const AuthRouteLayout = ({ path, exact, component, ...props }) => {
-    return (
-        <div className="auth-layout">
-            <div className="container">
-                <div className="bloc">
-                    <div className="Row">
-                        <div className="col">
-                            <Route
-                                path={path}
-                                exact={exact}
-                                component={component}
-                                {...props}
-                            />
+class AuthRouteLayout extends React.Component {
+    state = {
+        loading: true
+    };
+
+    componentWillMount() {
+        Tracker.autorun(() => {
+            this.isLoggedIn(this.props);
+        });
+    }
+
+    isLoggedIn(props) {
+        const logged = !Meteor.loggingIn() && !!Meteor.userId();
+
+        if (logged) {
+            props.history.push('/');
+        } else {
+            this.setState({ loading: false });
+        }
+    }
+
+    render() {
+        const { path, exact, component, ...props } = this.props;
+        const { loading } = this.state;
+
+        if (loading) {
+            return 'loading...';
+        }
+
+        return (
+            <div className="auth-layout">
+                <div className="container">
+                    <div className="bloc">
+                        <div className="Row">
+                            <div className="col">
+                                <Route
+                                    path={path}
+                                    exact={exact}
+                                    component={component}
+                                    {...props}
+                                />
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
-    );
-};
+        );
+    }
+}
 
 AuthRouteLayout.propTypes = {
     path: PropTypes.string.isRequired,
@@ -33,4 +65,4 @@ AuthRouteLayout.defaultProps = {
     exact: false
 };
 
-export default AuthRouteLayout;
+export default withRouter(AuthRouteLayout);
