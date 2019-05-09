98d314c34bed29e6ef4a9e65c363c0dafc68c117
Philip Nunoo <phanunoo@gmail.com>
2019-05-09 15:33:58 +0000

    Work on navbar

--- app/imports/ui/components/Navbar.jsx
+++ app/imports/ui/components/Navbar.jsx
@@ -1,71 +1,102 @@
-import React, { Fragment } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import { withRouter } from 'react-router';
+import { Meteor } from 'meteor/meteor';
 
-const Navbar = ({ location: { pathname } }) => {
-    const getNavLinkClass = path => (pathname === path ? 'active' : '');
-    const loggedIn = !!Meteor.userId();
+const Navbar = ({ logged, location: { pathname } }) => {
+    const getNavLinkClass = path => {
+        return pathname === path ? 'active' : '';
+    };
 
     return (
-        <div>
-            <nav className="navbar navbar-expand-lg navbar-light bg-light">
-                <a className="navbar-brand" href="#">
-                    Navbar w/ text
+        <nav
+            className="navbar navbar-expand-lg navbar-light fixed-top"
+            id="mainNav"
+        >
+            <div className="container">
+                <a className="navbar-brand" href="index.html">
+                    SuperDataScience
                 </a>
                 <button
-                    className="navbar-toggler"
+                    className="navbar-toggler navbar-toggler-right"
                     type="button"
                     data-toggle="collapse"
-                    data-target="#navbarText"
-                    aria-controls="navbarText"
+                    data-target="#navbarResponsive"
+                    aria-controls="navbarResponsive"
                     aria-expanded="false"
                     aria-label="Toggle navigation"
                 >
-                    <span className="navbar-toggler-icon" />
+                    Menu
+                    <i className="fas fa-bars" />
                 </button>
-                <div className="collapse navbar-collapse" id="navbarText">
-                    <ul className="navbar-nav mr-auto">
+                <div className="collapse navbar-collapse" id="navbarResponsive">
+                    <ul className="navbar-nav ml-auto">
                         <li className={`nav-item ${getNavLinkClass('/')}`}>
                             <Link className="nav-link" to="/">
                                 Home
                             </Link>
                         </li>
-                        <li className="nav-item">
-                            <a className="nav-link" href="#">
-                                Blogs
-                            </a>
-                        </li>
+                        {logged && (
+                            <React.Fragment>
+                                <li
+                                    className={`nav-item ${getNavLinkClass(
+                                        '/blogs'
+                                    )}`}
+                                >
+                                    <Link className="nav-link" to="/blogs">
+                                        Blogs
+                                    </Link>
+                                </li>
+                                <li className="nav-item">
+                                    <a
+                                        href="/"
+                                        className="nav-link"
+                                        onClick={() => Meteor.logout()}
+                                    >
+                                        Sign out
+                                    </a>
+                                </li>
+                            </React.Fragment>
+                        )}
+                        {!logged && (
+                            <React.Fragment>
+                                <li
+                                    className={`nav-item ${getNavLinkClass(
+                                        '/login'
+                                    )}`}
+                                >
+                                    <Link className="nav-link" to="/">
+                                        Login
+                                    </Link>
+                                </li>
+                                <li
+                                    className={`nav-item ${getNavLinkClass(
+                                        '/signup'
+                                    )}`}
+                                >
+                                    <Link className="nav-link" to="/signup">
+                                        Signup
+                                    </Link>
+                                </li>
+                            </React.Fragment>
+                        )}
                     </ul>
-                    <span className="navbar-text">
-                        <ul className="navbar-nav mr-auto">
-                            {!loggedIn && (
-                                <Fragment>
-                                    <li
-                                        className={`nav-item ${getNavLinkClass(
-                                            '/login'
-                                        )}`}
-                                    >
-                                        <Link className="nav-link" to="/">
-                                            Login
-                                        </Link>
-                                    </li>
-                                    <li
-                                        className={`nav-item ${getNavLinkClass(
-                                            '/signup'
-                                        )}`}
-                                    >
-                                        <Link className="nav-link" to="/signup">
-                                            Signup
-                                        </Link>
-                                    </li>
-                                </Fragment>
-                            )}
-                        </ul>
-                    </span>
                 </div>
-            </nav>
-        </div>
+            </div>
+        </nav>
     );
+};
+
+Navbar.propTypes = {
+    logged: PropTypes.bool,
+    location: PropTypes.shape({
+        pathname: PropTypes.string
+    }).isRequired
+};
+
+Navbar.defaultProps = {
+    logged: false
 };
 
 export default withRouter(Navbar);
